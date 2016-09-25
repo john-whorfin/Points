bc1dd5dafb5e2a498aa5386bbb3465422f5fbc59
john-whorfin <buckaroo@workermonkey.com>
2016-09-25 11:38:13 -0400

    add files

---
+++ pages/create.php
@@ -0,0 +1,26 @@
+
+
+<html>
+
+<link rel="stylesheet" type="text/css" href="./codeapp.css" media="screen" />
+<div>
+  <h2>Create item</h2>
+  <div class="input-list style-1 clearfix">
+
+      <input type="text" placeholder="New item">
+       <input id="testid" class="narrowbox" type="text" placeholder="pts">
+        <input type="checkbox" placeholder="repeat">
+               <input class="narrowbox" type="text" placeholder="max rpt">
+       <input class="submit" type="submit" placeholder="submit">
+  </div>
+
+<?php 
+	echo "thing";
+?>
+
+
+</div>
+
+
+
+</html>
