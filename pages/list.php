da6ca4c54c486f3a67fe38ec81bfce6632b979b4
john-whorfin <buckaroo@workermonkey.com>
2016-09-27 06:16:59 -0400

    A
    A

--- pages/list.php
+++ pages/list.php
@@ -2,14 +2,9 @@
 
 <link rel="stylesheet" type="text/css" href="/css/codeapp.css" media="screen" />
 <div>
-  <h2>Create item</h2>
+  <h2>List items</h2>
   <div class="input-list style-1 clearfix">
 
-      <input type="text" placeholder="New item">
-       <input id="testid" class="narrowbox" type="text" placeholder="pts">
-        <input type="checkbox" placeholder="repeat">
-               <input class="narrowbox" type="text" placeholder="max rpt">
-       <input class="submit" type="submit" placeholder="submit">
   </div>
 
 <?php
