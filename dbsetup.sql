2b566568fc818b069ba5071f595e7ef200e9cace
john-whorfin <buckaroo@workermonkey.com>
2016-10-01 10:15:46 -0400

    as
    as

--- dbsetup.sql
+++ dbsetup.sql
@@ -10,4 +10,6 @@ CREATE TABLE tasks (
 	points INT NOT NULL, 
 	repeat_day BOOL, max_repeat_day INT,  PRIMARY KEY (id));
 
+INSERT INTO tasks (name, points) VALUES("paint shed", 4),("shout at moon", 3);
+
 describe tasks;
--- html/list.php
+++ html/list.php
@@ -29,6 +29,9 @@ echo "fuck";
 }
 
 
+
+//  asdasd
+
 ?>
 
 
